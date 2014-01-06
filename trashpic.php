79141afacae4da7fd3a5fda78cb758d6a5905363
novalore <novalore@19.coop>
2014-01-06 03:26:42 +0100

    url autore in trashpic.php

--- trashpic.php
+++ trashpic.php
@@ -1,9 +1,10 @@
 <?php
 /*
 Plugin Name: TrashPic
-Plugin URI: http://19.coop
+Plugin URI: http://www.life-smile.eu
 Version: 0.0.1
 Author: Paolo Selis - Lorenzo Novaro
+Author URI: http://19.coop
 Description: Monitoring system
 */
 ?>
