f8d3f57bbbf1d2a798b68f3d19541b8ec53cc106
coma <p.selis@19.coop>
2014-02-04 14:03:18 +0100

    Aggiornate traduzioni e modificata la visualizzazione del custo post type

--- trashpic.php
+++ trashpic.php
@@ -60,7 +60,7 @@ function manage_trashpic_report_columns( $column, $post_id ){
 		case 'approved' :
 			/* Get the post meta. */
 			$col = get_post_meta( $post_id, $column, true );
-			if ( $col )
+			if ( $col == '1' )
 				echo _e('yes','TRASHPIC-plugin');
 			else
 				echo _e('no','TRASHPIC-plugin');
@@ -159,6 +159,103 @@ if(class_exists('Trashpic'))
     //wp_die($plugin);
 				add_filter("plugin_action_links_$plugin", 'plugin_settings_link');
 		add_action( 'wp_enqueue_scripts', 'register_plugin_styles' );
+		
+		function hide_publishing_actions(){
+			$my_post_type = 'trashpic-report';
+			global $post;
+			if($post->post_type == $my_post_type){
+				echo '
+                <style type="text/css">
+                    #misc-publishing-actions,
+                    #minor-publishing-actions{
+                        display:none;
+                    }
+                </style>
+            ';
+			}
+		}
+		add_action('admin_head-post.php', 'hide_publishing_actions');
+		add_action('admin_head-post-new.php', 'hide_publishing_actions');		
+		
+		function remove_quick_edit( $actions ) {
+			global $post;
+			if( $post->post_type == 'trashpic-report' ) {
+				unset($actions['edit'] );
+				unset($actions['view'] );
+				unset($actions['trash'] );
+				unset($actions['inline hide-if-no-js']);
+			}
+			return $actions;
+		}
+		
+		if (is_admin()) {
+			add_filter('post_row_actions','remove_quick_edit',10,2);
+		}		
+		
+		
+		add_filter( 'redirect_post_location', 'wpse_124132_redirect_post_location' );
+		/**
+		 * Redirect to the edit.php on post save or publish.
+		*/
+		function wpse_124132_redirect_post_location( $location ) {
+		
+			if ( isset( $_POST['save'] )  )
+				return admin_url( "edit.php?post_type=trashpic-report" );
+		
+			return $location;
+		}		
+		
+		add_action( 'views_edit-trashpic-report', 'remove_edit_post_views' );
+		function remove_edit_post_views( $views ) {
+			//unset($views['all']);
+			unset($views['publish']);
+			unset($views['pending']);
+			unset($views['trash']);
+			
+			$views['pre'] = '<a class="'.$class.'" href="'.admin_url().'edit.php?post_type=trashpic-report&pre=pre">Da approvare</a>';
+			$views['app'] = '<a class="'.$class.'" href="'.admin_url().'edit.php?post_type=trashpic-report&app=app">Approvate</a>';
+			$views['rif'] = '<a class="'.$class.'" href="'.admin_url().'edit.php?post_type=trashpic-report&rif=rif">Rifiutate</a>';
+			return $views;
+		}
+		
+		
+		add_action('pre_get_posts', 'my_special_list');
+		add_action('app_get_posts', 'my_special_list');
+		add_action('rif_get_posts', 'my_special_list');
+		
+		function my_special_list( $q ) {
+			if(is_admin()) $scr = get_current_screen();
+			if ( is_admin() && ( $scr->base === 'edit' ) && $q->is_main_query() ) {
+				// To target only a post type uncomment following line and adjust post type name
+				// if ( $scr->post_type !== 'post' ) return;
+				// if you change the link in function above adjust next line accordingly
+				$pre = filter_input(INPUT_GET, 'pre', FILTER_SANITIZE_STRING);
+				
+				if ( $pre === 'pre' ) {
+					// adjust meta query to fit your needs
+					$meta_query = array( 'key' => 'approved', 'value' =>'-1', );
+					$q->set( 'meta_query', array($meta_query) );
+				}
+				
+				$pre = filter_input(INPUT_GET, 'app', FILTER_SANITIZE_STRING);
+				
+				if ( $pre === 'app' ) {
+					// adjust meta query to fit your needs
+					$meta_query = array( 'key' => 'approved', 'value' => '1', );
+					$q->set( 'meta_query', array($meta_query) );
+				}
+				
+				$pre = filter_input(INPUT_GET, 'rif', FILTER_SANITIZE_STRING);
+				
+				if ( $pre === 'rif' ) {
+					// adjust meta query to fit your needs
+					$meta_query = array( 'key' => 'approved', 'value' => '0', );
+					$q->set( 'meta_query', array($meta_query) );
+				}
+				
+			}
+		}		
+		
 		
 	}
 }
